a764eebc0a9730c6adbfe1750f95129d94487ba2
Mehmet Efe Akca <efeakca@gmail.com>
2020-04-10 19:33:03 +0300

    Change timing so that LNMTL is hidden when one of the translators are done

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ async function main() {
         await sleepPromise(700)
     }
     if (settingsManager.lib.autoSwitchLNMTL) {
-        await Promise.all(translators)
+        await Promise.race(translators)
         uiManager.hideLNMTL()
     }
 }
