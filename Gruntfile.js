656f7d95beeadbf63db878353f484c67235a4801
Mehmet Efe Akca <efeakca@gmail.com>
2020-04-14 17:09:03 +0300

    Add production meta

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,20 @@
-const banner = `// ==UserScript==
+const banner = (addition) => `// ==UserScript==
 // @name         Translation Library
 // @namespace    lnmtltl
-// @version      0.1
+// @version      0.2
 // @description  Includes Sogou, Baido, Google Translate and more!
 // @author       mmtf
 // @match        https://lnmtl.com/chapter/*
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @require      https://userscripts-mirror.org/scripts/source/107941.user.js#sha384=Q8t880BurrlGKTdpvYv2+da12PYnvljdiU8aJvakk1uE3QMbzb190ueXNpAUY98p
+// @require      https://userscripts-mirror.org/scripts/source/107941.user.js#sha384=Q8t880BurrlGKTdpvYv2+da12PYnvljdiU8aJvakk1uE3QMbzb190ueXNpAUY98p${addition}
 // @license      MIT
 // ==/UserScript==
 `
+const productionMeta = `
+// @downloadURL https://openuserjs.org/install/mmtf/Translation_Library.user.js
+// @updateURL https://openuserjs.org/meta/mmtf/Translation_Library.meta.js`
+
 module.exports = (grunt) => {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
@@ -19,7 +23,7 @@ module.exports = (grunt) => {
                 src: "src/**/*.js",
                 dest: "./dist/translatelib.user.js",
                 options: {
-                    banner,
+                    banner: banner(productionMeta),
                     cacheFile: "./cache/prod.json",
                     browserifyOptions: {
                         debug: false,
@@ -43,7 +47,7 @@ module.exports = (grunt) => {
                 src: "src/**/*.js",
                 dest: "./dist/translatelib.dev.js",
                 options: {
-                    banner,
+                    banner: banner(),
                     cacheFile: "./cache/dev.json",
                     browserifyOptions: {
                         debug: true,
