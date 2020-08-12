49dc4ee0c2948153875521a972b1742ec9a0da6c
Mehmet Efe Akça <efeakca@gmail.com>
2020-08-12 13:53:54 +0300

    grunt(version): bump semiver minor

--- dist/translatelib.user.js
+++ dist/translatelib.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translation Library
 // @namespace    lnmtltl
-// @version      0.4.2
+// @version      0.4.3
 // @description  Includes Sogou, Baido, Google Translate and more!
 // @author       mmtf
 // @match        https://lnmtl.com/**
